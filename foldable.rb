a589b4ed370b609ea6d25cce81705e6595af40de
JeanYDiegoUSB <12-10848@usb.ve>
2016-11-19 14:55:49 -0400

    changes in both foldr
    -Array.foldr works with blocks (binary operations). (usable)
    -Rose.foldr works with blocks (binary operations). (usable)

--- foldable.rb
+++ foldable.rb
@@ -1,6 +1,8 @@
 class Array
-	def foldr e, b
-		self.reverse_each.inject(e,b)
+	def foldr e, &b
+		result = e
+		self.reverse_each {|elem| result = b.call(elem,result)}
+		result
 	end
 end
 class Rose
@@ -13,9 +15,15 @@ class Rose
 		@children.push elem
 		self
 	end
-	def foldr e, b
-		if self.children == [] then b.to_proc.call(self.elem,e)
-		else b.to_proc.call(self.elem,(self.children.map {|tree| tree.foldr(e,b)}).foldr(e,b))
-		end
+	def foldr e, &b
+		result = e
+		self.children.reverse_each {|tree| result = tree.foldr result, &b}
+		result = b.call(self.elem,result)
+		result
 	end
 end
+
+tree = Rose.new(1, [Rose.new(2, [ Rose.new(4), Rose.new(5) ]),Rose.new(3, [ Rose.new(6) ])])
+suma = tree.foldr(0) {|x,y| x + y}
+producto = tree.foldr(1) {|x,y| x * y}
+puts "La suma del árbol es #{suma} y su producto es #{producto}"
